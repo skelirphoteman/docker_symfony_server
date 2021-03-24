eca4f798aad73579622ab49b3934451f825f51d5
Julien Galoin <galoin.julien@gmail.com>
2021-03-24 17:21:34 +0100

    add symfony annotation and update route.yaml for admin et core controller

--- www/src/Http/Controller/Core/CoreController.php
+++ www/src/Http/Controller/Core/CoreController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controller;
+namespace App\Http\Controller\Core;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
